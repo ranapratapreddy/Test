e694f0a62eaa2ebba6594b83d9428d1f39763d20
ranapratapreddy <ranareddy716@gmail.com>
2024-11-18 18:15:56 +0530

    Even and  Odd

---
+++ index.js
@@ -0,0 +1,11 @@
+const numbers = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,];
+let a=arr.length
+
+for (let i=0, i <= a-1,i++){
+    if (arr[i]%2==0){
+        console.log(arr[i])
+    }else{
+        console.log(a[i])
+    }
+}
+ 
